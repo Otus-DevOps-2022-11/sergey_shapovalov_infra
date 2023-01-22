f596cd837566fe1d5237d4f646fca364277cb5ab
ssh500 <73992346+ssh500@users.noreply.github.com>
2023-01-22 14:50:34 +0300

    Create install_mongodb.sh

---
+++ config- scripts/install_mongodb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+apt-get update; sudo apt-get -y install mongodb-org
+
+systemctl enable --now mongod
+
+systemctl status mondod
