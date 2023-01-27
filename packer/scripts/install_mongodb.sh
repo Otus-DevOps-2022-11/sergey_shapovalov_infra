85439f4d0db3cd8e658a96d75fd5f438f8b000d5
ssh500 <73992346+ssh500@users.noreply.github.com>
2023-01-27 14:26:13 +0300

    Update install_mongodb.sh

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -11,12 +11,9 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
-#wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-
-
 apt-get update; apt-get -y install mongodb-org
 
 systemctl enable --now mongod
 
 systemctl status mongod
+
